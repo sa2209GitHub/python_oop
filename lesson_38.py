667305284636d740ff5ce3f03f5364b629cd851f
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-02-26 19:26:13 +0300

    Added some examples

--- lesson_38.py
+++ lesson_38.py
@@ -2,3 +2,137 @@
 #   38
 #
 # Introduction to Data Classes. Part 2
+#
+# https://docs.python.org/3/library/dataclasses.html
+
+
+import math
+from dataclasses import dataclass, field, InitVar
+
+
+# Regular class (Example 1)
+
+
+class Vector3D:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+        # self.length = (x * x + y * y + z * z) ** 0.5
+        self.length = math.sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+
+
+v3d = Vector3D(10, 20, 30)
+print(v3d.__dict__)
+del v3d, Vector3D
+
+
+# Regular class (Example 2)
+
+
+class Vector3D:
+    def __init__(self, x: int, y: int, z: int, calc_len: bool = True):
+        self.x = x
+        self.y = y
+        self.z = z
+        # self.length = (x * x + y * y + z * z) ** 0.5
+        self.length = math.sqrt(
+            pow(x, 2) + pow(y, 2) + pow(z, 2)) if calc_len else 0
+
+
+v3d = Vector3D(10, 20, 30)
+print(v3d.__dict__)
+del v3d, Vector3D
+
+
+# Data class (Example 1)
+
+
+@dataclass
+class Vector3D:
+    x: int
+    y: int
+    z: int
+    length: float = field(init=False)
+
+    def __post_init__(self):
+        self.length = math.sqrt(
+            pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+
+v3d = Vector3D(10, 20, 30)
+print(v3d.__dict__)
+del v3d, Vector3D
+
+
+# Data class (Example 2)
+
+
+@dataclass
+class Vector3D:
+    x: int = field(repr=False)
+    y: int
+    z: int = field(compare=False)
+    length: float = field(init=False, compare=False)
+
+    def __post_init__(self):
+        self.length = math.sqrt(
+            pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+
+v3d = Vector3D(10, 20, 30)
+v3d2 = Vector3D(10, 20, 50)
+print(v3d.__dict__)
+print(v3d2.__dict__)
+print(v3d == v3d2)
+del v3d, Vector3D
+
+
+# Data class (Example 3)
+
+
+@dataclass
+class Vector3D:
+    x: int
+    y: int
+    z: int
+    calc_len: InitVar[bool] = True
+    length: float = field(init=False, compare=False, default=0)
+
+    def __post_init__(self, calc_len: bool):
+        if calc_len:
+            self.length = math.sqrt(
+                pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+
+v3d = Vector3D(10, 20, 30)
+v3d2 = Vector3D(10, 20, 50, False)
+print(v3d.__dict__)
+print(v3d2.__dict__)
+print(v3d, v3d2, sep='\n')
+del v3d, Vector3D
+
+
+# Data class parameters (Example 4)
+
+
+@dataclass(repr=True, eq=True, order=True)
+class Vector3D:
+    x: int
+    y: int
+    z: int
+    calc_len: InitVar[bool] = True
+    length: float = field(init=False, compare=False, default=0)
+
+    def __post_init__(self, calc_len: bool):
+        if calc_len:
+            self.length = math.sqrt(
+                pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+
+v3d = Vector3D(10, 20, 30)
+v3d2 = Vector3D(10, 20, 30)
+print(v3d.__dict__)
+print(v3d2.__dict__)
+print(v3d >= v3d2)
+del v3d, Vector3D
---
+++ lesson_39.py
@@ -0,0 +1,113 @@
+#
+#   39
+#
+# Using Data Classes in Inheritance
+# and 'make_dataclass()' Method
+#
+# https://docs.python.org/3/library/dataclasses.html
+
+
+# Example 1
+
+
+from dataclasses import dataclass, field, InitVar, make_dataclass
+from typing import Any
+
+
+class GoodsMethodsFactory:
+    @staticmethod
+    def get_init_measure():
+        return [0, 0, 0]
+
+
+@dataclass
+class Goods:
+    current_uid = 0
+
+    uid: int = field(init=False)
+    price: Any = None
+    weight: Any = None
+
+    def __post_init__(self):
+        print("Goods: __post_init__() method was called")
+        Goods.current_uid += 1
+        self.uid = Goods.current_uid
+
+
+@dataclass
+class Book(Goods):
+    title: str = ""
+    author: str = ""
+    price: float = 0
+    weight: int | float = 0
+    measure: list = field(default_factory=GoodsMethodsFactory.get_init_measure)
+
+    def __post_init__(self):
+        super().__post_init__()
+        print("Book: __post_init() method was called")
+
+
+b = Book()
+b2 = Book(1000, 100, "Pythop OOP", "Programmer Joe")
+b3 = Book(1024, 128, "Just Book", "Just Name")
+b4 = Book(1024, 128, "Just Book", "Just Name", [1, 2, 3])
+
+print(b)
+print(b2)
+print(b3)
+print(b4)
+
+
+# Example 2 ('make_dataclass()' method)
+
+
+class Car:
+    def __init__(self, brand: str, model: str, year_of_issue: int, max_speed: int, engine_hp: int, price: int):
+        self.brand = brand
+        self.model = model
+        self.year_of_issue = year_of_issue
+        self.max_speed = max_speed
+        self.engine_hp = engine_hp
+        self.price = price
+
+    def get_info(self):
+        return {
+            'brand': self.brand,
+            'model': self.model,
+            'year_of_issue': self.year_of_issue,
+            'max_speed': self.max_speed,
+            'engine_hp': self.engine_hp,
+            'price': self.price}
+
+
+car = Car(brand='Toyota', model='Camry', year_of_issue=2021,
+          max_speed=210, engine_hp=180, price=12000)
+print(car.get_info())
+del car, Car
+
+
+# The same class using 'make_dataclass()' method
+
+
+Car = make_dataclass("Car", [('brand', str),
+                             ('model', str),
+                             ('year_of_issue', int),
+                             ('max_speed', int),
+                             ('engine_hp', int),
+                             ('price', int, field(default=0))
+                             ],
+                     namespace={'get_info': lambda self: {
+                         'brand': self.brand,
+                         'model': self.model,
+                         'year_of_issue': self.year_of_issue,
+                         'max_speed': self.max_speed,
+                         'engine_hp': self.engine_hp,
+                         'price': self.price
+                     }
+})
+
+
+car = Car(brand='BMW', model='X6', year_of_issue=2022,
+          max_speed=250, engine_hp=280, price=99000)
+print(car.get_info())
+del car, Car
